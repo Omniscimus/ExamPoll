7e6a2852f8242844c683b821111370eabc1e3b9b
Omniscimus <omniscimus@icloud.com>
2015-11-27 21:37:34 +0100

    Use POST instead of GET for reCAPTCHA

--- Vote_Validator.php
+++ Vote_Validator.php
@@ -31,7 +31,17 @@ class Vote_Validator {
      */
     private function captcha_is_valid($g_recaptcha_response) {
         $config = include 'config.php';
-        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $config["secret-key"] . "&response=" . $g_recaptcha_response);
+        $data = array('secret' => $config["secret-key"], 'response' => $g_recaptcha_response);
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
         $decoded_response = json_decode($response, true);
         return $decoded_response["success"];
     }
