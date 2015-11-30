13904b8b797643e2f9ff1af6ff0dfe6d7873312c
Nick Belzer <nickbelzer@gmail.com>
2015-11-30 22:39:38 +0100

    Made some styling changes to poll.php for login and voting page
    Included some basic markup for the login page. The login page now contains a centered login screen with a nice text field and button for the user to enter their code.
    
    Included some basic markup for the voting page, all the possible votes are now displayed in boxes with a description of the item below it. Everything is alligned in the middle and looks a bit better this way. I moved the options to a different file where you can modify an array of items with a name and a description key. This file is then included and the options can be read out of the array. Since the options started with id=1 I left it that way.

---
+++ php/options.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+  ["name" => "The Fellowship of the Exams",
+    "description" => "Description"],
+  ["name" => "The Exams Awaken",
+    "description" => "Description"],
+  ["name" => "Fifty Shades of Exams",
+    "description" => "Description"],
+  ["name" => "The Dark Exam Rises",
+    "description" => "Because I'm Batman!"],
+  ["name" => "Exception",
+    "description" => "Don't you dare to press the red button"]
+);
--- php/poll.php
+++ php/poll.php
@@ -7,57 +7,128 @@ $mode = $validator->get_response_mode();
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Thema laatste schooldag</title>
-        <?php if ($mode === 0): ?>
-            <script src='https://www.google.com/recaptcha/api.js'></script>
-        <?php endif; ?>
-    </head>
-    <body>
-        <?php if ($mode === 0): ?>
-            <div>
-                <h1>Stem op een thema:</h1>
-                <form method="post"
-                      action="poll.php">
-                    <input type="radio" name="vote" value="1" />The Fellowship of the Examen<br />
-                    <input type="radio" name="vote" value="2" />The Examen Awakens<br />
-                    <input type="radio" name="vote" value="3" />Fifty Shades of Examens<br />
-                    <input type="radio" name="vote" value="4" />The Dark Examen Rises<br />
-                    <input type="radio" name="vote" value="5" />Exception<br />
-                    <input type="hidden" name="code" value="<?php echo $_GET["code"]; ?>" />
-                    <?php $config = include 'config.php'; ?>
-                    <div class="g-recaptcha" data-sitekey="<?php echo $config["site-key"] ?>"></div>
-                    <input type="submit" value="Stem" />
-                </form>
+  <head>
+    <title>Thema laatste schooldag</title>
+    <?php if ($mode === 0): ?>
+      <script src='https://www.google.com/recaptcha/api.js'></script>
+    <?php endif; ?>
+    <link type="text/css" rel="stylesheet" href="/foundation/css/foundation.css">
+    <link type="text/css" rel="stylesheet" href="/foundation/css/exampoll.css">
+  </head>
+  <body>
+    <?php if ($mode === 0): ?>
+      <!-- Vote page -->
+
+      <!-- Header, title -->
+      <div class="row">
+        <div class="medium-10 medium-centered columns">
+          <h3>Stem op een thema</h3>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="medium-10 medium-centered columns">
+          <!-- Explanation -->
+          <div class="row">
+            <div class="small-12 columns">
+              <p>
+              <h5>Werking</h5>
+              Voor het eind examen van het schooljaar 2015-2016 kunt u uw keuze
+              aangeven voor een mogelijke eindexamenstunt. Hieronder ziet u de
+              keuzes. U kunt er maximaal 1 aanvinken.
+              </p>
             </div>
-        <?php elseif ($mode === 1): ?>
-            <div>
-                <h1>Stemcode ongeldig</h1>
-                <p>Je stemcode is ongeldig. Dit kan betekenen dat je al gestemd 
-                    hebt, of dat je een verkeerde code hebt ingevuld.</p>
+          </div>
+          <form method="post">
+          <!-- The options -->
+          <?php
+            $options = include "options.php";
+
+            $i = 1;
+            foreach ($options as $option)
+            { ?>
+            <div class="row">
+              <div class="small-12 columns votingItemHeader">
+                <input required type="radio" name="vote" value="<?php echo
+                $i;
+                ?>">
+                <label><h4><? echo $option['name']; ?></h4></label>
+              </div>
+              <div class="medium-12 columns votingItemDescription"
+                   style="padding-left: 2em;">
+                <p><? echo $option['description']; ?></p>
+              </div>
             </div>
-        <?php elseif ($mode === 2): ?>
-            <div>
-                <h1>Stemcode invullen</h1>
-                <form method="get" action="poll.php">
-                    Stemcode: <input type="text" name="code" />
-                    <input type="submit" value="Zoek" />
-                </form>
+              <?php
+              $i++;
+            }
+          ?>
+           <div class="row">
+             <div class="small-12 columns"
+                  style="margin-top: 1em">
+               <input type="hidden" name="code" value="<?php echo $_GET["code"]; ?>" />
+               <?php $config = include 'config.php'; ?>
+               <label>
+                 <h5>Captcha Code</h5>
+                 <div class="g-recaptcha" data-sitekey="<?php echo $config["site-key"] ?>"></div>
+               </label>
+               <p style="margin-top: 1em; margin-bottom: 0.5em;">
+                 Als u zeker weet dat u de juiste keuze heeft gemaakt en
+                 als u de captcha code correct hebt ingevoerd kunt u op de
+                 stem knop klikken.
+               </p>
+               <input type="submit" value="Stem" class="button"
+                      style="padding: 1em 4em; margin-top: 0.2em;">
+
+             </div>
+           </div>
+        </form>
+        </div>
+      </div>
+    <?php elseif ($mode === 1): ?>
+      <div>
+        <h1>Stemcode ongeldig</h1>
+        <p>Je stemcode is ongeldig. Dit kan betekenen dat je al gestemd
+            hebt, of dat je een verkeerde code hebt ingevuld.</p>
+      </div>
+    <?php elseif ($mode === 2): ?>
+      <!-- Enter code page -->
+
+      <div class="loginBackground">
+        <div class="row">
+          <div class="medium-7 large-5 medium-centered columns">
+            <div class="login">
+
+              <form method="get" action="poll.php">
+                <h3>Stemcode invullen</h3>
+
+                <label style="color:white;">
+                  Stemcode:
+                  <input type="text" name="code" />
+                </label>
+
+                <input type="submit" value="Zoek" class="button"/>
+              </form>
+
             </div>
-        <?php elseif ($mode === 3): ?>
-            <div>
-                <h1>Stem ontvangen</h1>
-                <p>Bedankt voor je stem!</p>
-            </div>
-        <?php elseif ($mode === 4): ?>
-            <div>
-                <h1>Wow.</h1>
-                <p>Volgens mij bent u een 1337 h4xx0r. Probeer het nog eens.</p>
-                <img src="http://stream1.gifsoup.com/view3/1752198/never-gonna-give-you-up-o.gif" 
-                     alt="WOW" />
-            </div>
-        <?php endif; ?>
-    </body>
+          </div>
+        </div>
+      </div>
+    <?php elseif ($mode === 3): ?>
+      <div>
+        <h1>Stem ontvangen</h1>
+        <p>Bedankt voor je stem!</p>
+      </div>
+    <?php elseif ($mode === 4): ?>
+      <div>
+        <h1>Wow.</h1>
+        <p>Volgens mij bent u een 1337 h4xx0r. Probeer het nog eens.</p>
+        <img src="http://stream1.gifsoup.com/view3/1752198/never-gonna-give-you-up-o.gif"
+             alt="WOW" />
+      </div>
+    <?php endif; ?>
+  </body>
 </html>
 
 <?php
