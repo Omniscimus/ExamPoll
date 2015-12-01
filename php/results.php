3ccccf3d907711e6b7d2d227ac3e24a7693c40e7
Omniscimus <omniscimus@icloud.com>
2015-12-01 22:19:56 +0100

    Add POST password handling to results page

--- php/results.php
+++ php/results.php
@@ -1,3 +1,17 @@
+<?php
+if (is_string($_POST["password"])) {
+    $config = include 'config.php';
+    if ($_POST["password"] == $config["results-password"]) {
+        $mode = 0; // Correct password
+        require_once 'sql/MySQL_Manager.php';
+    } else {
+        $mode = 1; // Wrong password
+    }
+} else {
+    $mode = 2; // No password yet
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,21 +19,37 @@
     </head>
     <body>
         <h1>Resultaten</h1>
-        <?php if ($mode === 0): ?>
+        <?php if ($mode !== 0): ?>
             <form action="results.php" method="post">
+                <?php if ($mode === 1): ?>
+                    <img src="http://cdn.meme.li/instances/500x/44412533.jpg"
+                         alt="Rekt" /><br />
+                <?php endif; ?>
                 Wachtwoord: <input type="password" name="password" />
                 <input type="submit" />
             </form>
-        <?php elseif ($mode === 1): ?>
+        <?php else: ?>
             <table>
+                <tr>
+                    <th>Optie</th>
+                    <th>Stemmen</th>
+                </tr>
                 <?php
                 $mySQL = new MySQL_Manager();
                 $results = $mySQL->getExamPoll_SQL()->get_results();
                 $mySQL->closeConnection();
+
+                $vote_options = include 'options.php';
                 foreach ($results as $option) {
                     echo "<tr>";
-                    foreach ($option as $column) {
-                        echo "<td>$column</td>";
+                    for ($i = 0; $i < count($option); $i++) {
+                        echo "<td>";
+                        if ($i === 0) {
+                            echo $vote_options[$option[0]]["name"];
+                        } else {
+                            echo $option[$i];
+                        }
+                        echo "</td>";
                     }
                     echo "</tr>";
                 }
